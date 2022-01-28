6aa73ae2b47ae4b33ad02c3be55db3d4722a95b1
zc402 <zc35402@163.com>
2022-01-28 12:34:44 +0800

    training code refactoring

--- taper/train.py
+++ taper/train.py
@@ -12,22 +12,22 @@ from taper.network import TAPER
 
 # joint xy coords -> gcn -> fcn
 class Trainer:
-    def __init__(self):
-        self.cfg = get_cfg_defaults()
-        self.cfg.merge_from_file(Path('configs', 'train.yaml'))
-        self.data_loader = self.train_data_loader(self.cfg)
-        self.model = self.train_model(self.cfg)
+    def __init__(self, dataloader, model, ckpt, device):
+
+        self.data_loader = dataloader
+        self.model = self._load_ckpt(model, ckpt, device)
+        self.ckpt = ckpt
+        self.device = device
         self.loss = CrossEntropyLoss()  # The input is expected to contain raw, unnormalized scores for each class.
         self.opt = optim.Adam(self.model.parameters(), lr=1e-3)
         self.logger = logging.getLogger(__name__)
 
     def train(self):
-        device = torch.device(self.cfg.MODEL.DEVICE)
         step = 1
         for epoch in range(100):
             for train_data in self.data_loader:
-                tensor_NCTV = train_data['tensor_ctv'].to(device)  # Batch is N dim
-                label_NT = train_data['label_t'].to(device)  # N,T
+                tensor_NCTV = train_data['tensor_ctv'].to(self.device)  # Batch is N dim
+                label_NT = train_data['label_t'].to(self.device)  # N,T
 
                 class_out = self.model(tensor_NCTV)  # Out: N*T,C
                 label_NT = label_NT.reshape([-1])  # N*T
@@ -44,38 +44,29 @@ class Trainer:
                     print("Accuracy: {:.2f}".format(acc))
 
                 if step % 2000 == 0:
-                    self.save_ckpt(self.cfg, self.model)
-                    print('Model save')
+                    self._save_ckpt(self.model, self.ckpt)
+
                 step = step + 1
 
     @staticmethod
-    def train_data_loader(cfg):
-        concat_dataset = ConcatVideo.from_config(cfg)
-        train_loader = DataLoader(concat_dataset, batch_size=cfg.MODEL.BATCH_SIZE, shuffle=True, drop_last=True)
-        return train_loader
+    def _load_ckpt(model, ckpt, device):
 
-    @staticmethod
-    def train_model(cfg):
-
-        model = TAPER.from_config(cfg)
-        ckpt_path = Path(cfg.DATA_ROOT) / cfg.MODEL.CKPT_DIR / cfg.MODEL.TAPER_CKPT
-        if ckpt_path.is_file():
+        if ckpt.is_file():
             print("Resume from previous ckeckpoint")
-            ckpt = torch.load(ckpt_path)
+            ckpt = torch.load(ckpt)
             model.load_state_dict(ckpt)
         else:
             print("Previous checkpoint not found.")
             print("Start the training from scratch!")
-            ckpt_path.parent.mkdir(exist_ok=True)
+            ckpt.parent.mkdir(exist_ok=True)
         model.train()
-        device = torch.device(cfg.MODEL.DEVICE)
         model.to(device)
         return model
 
     @staticmethod
-    def save_ckpt(cfg, model):
-        save_path = Path(cfg.DATA_ROOT) / cfg.MODEL.CKPT_DIR / cfg.MODEL.TAPER_CKPT
-        torch.save(model.state_dict(), save_path)
+    def _save_ckpt(model, ckpt):
+        torch.save(model.state_dict(), ckpt)
+        print('Model save')
 
     @staticmethod
     def acc(input, target):
@@ -83,5 +74,22 @@ class Trainer:
         acc = (class_N == target).float().mean().item()
         return acc
 
+    @classmethod
+    def _build_data_loader(cls, cfg):
+        concat_dataset = ConcatVideo.from_config(cfg)
+        train_loader = DataLoader(concat_dataset, batch_size=cfg.MODEL.BATCH_SIZE, shuffle=True, drop_last=True)
+        return train_loader
+
+    @classmethod
+    def from_config(cls, cfg):
+        data_loader = cls._build_data_loader(cfg)
+        model = TAPER.from_config(cfg)
+        device = torch.device(cfg.MODEL.DEVICE)
+        ckpt = Path(cfg.DATA_ROOT) / cfg.MODEL.CKPT_DIR / cfg.MODEL.TAPER_CKPT
+        instance = Trainer(data_loader, model, ckpt, device)
+        return instance
+
 if __name__ == '__main__':
-    Trainer().train()
+    train_cfg = get_cfg_defaults()
+    train_cfg.merge_from_file(Path('configs', 'train.yaml'))
+    Trainer.from_config(train_cfg).train()
